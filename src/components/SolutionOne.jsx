78ae449d7883e14fc7f15e2818c593a4ca77d5c6
Arham Khawar <68909415+ch-Arham@users.noreply.github.com>
2023-06-08 19:26:31 +0500

    solution one completed

--- src/components/SolutionOne.jsx
+++ src/components/SolutionOne.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react"
+
+// importing original images
 import img_1 from "../assets/images/original/img_1.jpg"
 import img_2 from "../assets/images/original/img_2.jpg"
 import img_3 from "../assets/images/original/img_3.jpg"
@@ -33,25 +36,173 @@ import img_14_small from "../assets/images/scaled/img_14_small.jpg"
 import img_15_small from "../assets/images/scaled/img_15_small.jpg"
 import img_16_small from "../assets/images/scaled/img_16_small.jpeg"
 
+
 const SolutionOne = () => {
+    useEffect(() => {
+        const blurDivs = document.querySelectorAll('.blur-load')
+
+        blurDivs.forEach((div) => {
+            const img = div.querySelector('img')
+
+            const loaded = () => {
+                div.classList.add('loaded')
+                img.removeEventListener('load', loaded);
+            }
+
+            if (img.complete) {
+                loaded()
+            } else {
+                img.addEventListener('load', loaded)
+            }
+        })
+    }, [])
+
+
   return (
     <div className='grid'>
-        <img src={img_1} alt='img_1' loading='lazy' />
-        <img src={img_2} alt='img_2' loading='lazy' />
-        <img src={img_3} alt='img_3' loading='lazy' />
-        <img src={img_4} alt='img_4' loading='lazy' />
-        <img src={img_5} alt='img_5' loading='lazy'/>
-        <img src={img_6} alt='img_6' loading='lazy' />
-        <img src={img_7} alt='img_7' loading='lazy' />
-        <img src={img_8} alt='img_8' loading='lazy' />
-        <img src={img_9} alt='img_9' loading='lazy' />
-        <img src={img_10} alt='img_10' loading='lazy' />
-        <img src={img_11} alt='img_11' loading='lazy' />
-        <img src={img_12} alt='img_12' loading='lazy' />
-        <img src={img_13} alt='img_13' loading='lazy' />
-        <img src={img_14} alt='img_14' loading='lazy' />
-        <img src={img_15} alt='img_15' loading='lazy' />
-        <img src={img_16} alt='img_16' loading='lazy' />
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_1_small})`,
+            }}
+        >
+            <img src={img_1} alt='img_1' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_2_small})`,
+            }}
+        >
+            <img src={img_2} alt='img_2' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_3_small})`,
+            }}
+        >
+            <img src={img_3} alt='img_3' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_4_small})`,
+            }}
+        >
+            <img src={img_4} alt='img_4' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_5_small})`,
+            }}
+        >
+            <img src={img_5} alt='img_5' loading='lazy'/>
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_6_small})`,
+            }}
+        >
+            <img src={img_6} alt='img_6' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_7_small})`,
+            }}
+        >
+            <img src={img_7} alt='img_7' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_8_small})`,
+            }}
+        >
+            <img src={img_8} alt='img_8' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_9_small})`,
+            }}
+        >
+            <img src={img_9} alt='img_9' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_10_small})`,
+            }}
+        >
+            <img src={img_10} alt='img_10' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_11_small})`,
+            }}
+        >
+            <img src={img_11} alt='img_11' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_12_small})`,
+            }}
+        >
+            <img src={img_12} alt='img_12' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_13_small})`,
+            }}
+        >
+            <img src={img_13} alt='img_13' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_14_small})`,
+            }}
+        >
+            <img src={img_14} alt='img_14' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_15_small})`,
+            }}
+        >
+            <img src={img_15} alt='img_15' loading='lazy' />
+        </div>
+
+        <div 
+            className="blur-load"
+            style={{
+                backgroundImage: `url(${img_16_small})`,
+            }}
+        >
+            <img src={img_16} alt='img_16' loading='lazy' />
+        </div>
     </div>
   )
 }
